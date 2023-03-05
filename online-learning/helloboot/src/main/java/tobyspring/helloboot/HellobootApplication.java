31593bb109680f27255a7f5579d84c3b6c284bd4
hoa0217 <gjwjdghk123@naver.com>
2023-03-05 20:14:26 +0900

    [ONLINE] 토비의 스프링 부트 - (섹션4)SpringBootApplication 커스텀

--- online-learning/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ online-learning/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,12 +1,10 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -24,26 +22,8 @@ public class HellobootApplication {
   }
 
   public static void main(String[] args) {
-    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-      @Override
-      protected void onRefresh() {
-        super.onRefresh();
-
-        ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-        //dispatcherServlet.setApplicationContext(this); 없어도 잘 동작한다. -> 이해하려면 life사이클을 봐야한다.
-
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-          servletContext.addServlet("dispatcherServlet", dispatcherServlet
-          ).addMapping("/*");
-        });
-        webServer.start();
-      }
-    };
-    applicationContext.register(HellobootApplication.class);
-    applicationContext.refresh();
+    MySpringApplication.run(HellobootApplication.class, args);
   }
-
 }
 
 
