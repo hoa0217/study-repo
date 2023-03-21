ea7a49bb339d6e1dd8e8d17f18550fc1c5b81a27
hoa0217 <gjwjdghk123@naver.com>
2023-03-22 02:02:51 +0900

    [BOOK] TDD - 15장 정리 완료

--- book/tdd/src/main/java/tdd/Sum.java
+++ book/tdd/src/main/java/tdd/Sum.java
@@ -1,17 +1,21 @@
 package tdd;
 
-public class Sum implements Expression{
+public class Sum implements Expression {
 
-  Money augend;
-  Money addend;
+  Expression augend;
+  Expression addend;
 
-  public Sum(Money augend, Money addend) {
+  public Sum(Expression augend, Expression addend) {
     this.augend = augend;
     this.addend = addend;
   }
 
-  public Money reduce(Bank bank, String to){
-    int amount = addend.amount + augend.amount;
+  public Money reduce(Bank bank, String to) {
+    int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
     return new Money(amount, to);
+  }
+
+  public Expression plus(Expression addend) {
+    return null;
   }
 }
