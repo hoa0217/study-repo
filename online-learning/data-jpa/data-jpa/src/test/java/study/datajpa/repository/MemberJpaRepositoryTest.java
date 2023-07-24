1c74089d3b478e6d7cd50e4c399ed7c9b9671595
hoa0217 <gjwjdghk123@naver.com>
2023-07-25 02:22:30 +0900

    [ONLINE] 실전!스프링 데이터 JPA - (섹션3)공통 인터페이스 적용

--- online-learning/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ online-learning/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -37,7 +37,7 @@ class MemberJpaRepositoryTest {
     memberJpaRepository.save(member1);
     memberJpaRepository.save(member2);
 
-    // 단건조회
+    // 단건조회 검증
     Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
     Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
     assertThat(findMember1).isEqualTo(member1);
