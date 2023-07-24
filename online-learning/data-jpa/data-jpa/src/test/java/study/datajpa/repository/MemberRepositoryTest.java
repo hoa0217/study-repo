92695769e529792c9ba01eedb22e24e0f10875a2
hoa0217 <gjwjdghk123@naver.com>
2023-07-25 02:18:53 +0900

    [ONLINE] 실전!스프링 데이터 JPA - (섹션3)공통 인터페이스 설정

--- online-learning/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ online-learning/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -19,6 +19,7 @@ class MemberRepositoryTest {
 
   @Test
   public void save() {
+    System.out.println("memberRepository = " + memberRepository.getClass()); // memberRepository = class com.sun.proxy.$Proxy122
     Member member = new Member("memberA");
     Member savedMember = memberRepository.save(member);
 
