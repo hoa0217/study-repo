b924cecf24b956c2bb1580ca0202e5049d9d5cc7
hoa0217 <gjwjdghk123@naver.com>
2023-07-26 20:31:41 +0900

    [ONLINE] 실전!스프링 데이터 JPA - (섹션4)메소드 이름으로 쿼리 생성

--- online-learning/data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ online-learning/data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -7,4 +7,9 @@ import study.datajpa.entity.Member;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
   List<Member> findByUsername(String username);
+
+  // 파라미터가 2개가 넘어가면 메서드가 너무 길어진다. 이때는 직접 짜도록하자.
+  List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+  List<Member> findTop3HelloBy();
 }
