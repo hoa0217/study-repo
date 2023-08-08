fb204ac4ec4d1c0255e5a0034805104f2f23a3c9
hoa0217 <gjwjdghk123@naver.com>
2023-08-09 07:38:13 +0900

    [ONLINE] 실전! Querydsl - JPQL vs Querydsl

--- online-learning/querydsl/querydsl/src/test/java/study/querydsl/entity/MemberTest.java
+++ online-learning/querydsl/querydsl/src/test/java/study/querydsl/entity/MemberTest.java
@@ -2,23 +2,20 @@ package study.querydsl.entity;
 
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
-@Commit
 class MemberTest {
 
   @Autowired
   EntityManager em;
 
   @Test
-  public void member(){
+  public void member() {
     Team teamA = new Team("teamA");
     Team teamB = new Team("teamB");
     em.persist(teamA);
