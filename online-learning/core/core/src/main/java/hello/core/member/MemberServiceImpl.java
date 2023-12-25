99b8516d5c50d73255ffa0ac2838c43a628c1594
hoa0217 <gjwjdghk123@naver.com>
2023-12-25 20:11:22 +0900

    [ONLINE] 스프링 핵심 원리 기본편 - 관심사의 분리

--- online-learning/core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ online-learning/core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,9 +3,13 @@ package hello.core.member;
 // 구현체가 하나만 있을 때 Impl붙이는게 관례
 public class MemberServiceImpl implements MemberService {
 
-    // 다형성에 의해 MemoryMemberRepository 호출
-    // 의존관계가 인터페이스 뿐만 아니라 구현까지 모두 의존하는 문제점이 있음
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // 의존관계에 대한 고민은 외부에 맡기고 실행에만 집중.
+    // 의존관계 주입(Dependency Injection)
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- online-learning/core/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ online-learning/core/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -2,14 +2,22 @@ package hello.core.member;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MemberServiceTest {
 
-    private MemberService memberService = new MemberServiceImpl();
+    private MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
-    public void join(){
+    public void join() {
         // given
         Member member = new Member(1L, "memberA", Grade.VIP);
 
