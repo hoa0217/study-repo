27dfb2a288e9683dca0959ed2d01354a143516b8
hoa0217 <gjwjdghk123@naver.com>
2024-01-01 20:22:49 +0900

    [ONLINE] 스프링 핵심 원리 기본편 - 스프링으로 전환하기

--- online-learning/core/core/src/main/java/hello/core/AppConfig.java
+++ online-learning/core/core/src/main/java/hello/core/AppConfig.java
@@ -8,25 +8,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-/**
- * 객체의 생성과 연결은 `AppConfig` 가 담당한다.
- */
+// 스프링 컨테이너 설정정보
+@Configuration
 public class AppConfig {
 
+    // Bean으로 등록된 객체를 스프링 컨테이너에서 관리
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 }
