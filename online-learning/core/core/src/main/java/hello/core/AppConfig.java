1814b69910625686b0e64766c9cbe42bbdeba17c
hoa0217 <gjwjdghk123@naver.com>
2024-01-15 12:30:39 +0900

    [ONLINE] 스프링 핵심 원리 기본편 - `@Configuration`과 싱글톤

--- online-learning/core/core/src/main/java/hello/core/AppConfig.java
+++ online-learning/core/core/src/main/java/hello/core/AppConfig.java
@@ -18,16 +18,19 @@ public class AppConfig {
     // Bean으로 등록된 객체를 스프링 컨테이너에서 관리
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
