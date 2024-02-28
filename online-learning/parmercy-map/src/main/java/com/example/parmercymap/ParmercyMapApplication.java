814fcd5e7f5b605961a533da1df576cb500e5e8e
hoa0217 <gjwjdghk123@naver.com>
2024-02-28 23:33:22 +0900

    [ONLINE] 약국길찾기 프로젝스 생성

---
+++ online-learning/parmercy-map/src/main/java/com/example/parmercymap/ParmercyMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.parmercymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParmercyMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParmercyMapApplication.class, args);
+	}
+
+}
